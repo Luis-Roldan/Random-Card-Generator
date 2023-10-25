e5ef4ddae2596de22c8723944d2c802f1cc9215b
Luis-Roldan <108189366+Luis-Roldan@users.noreply.github.com>
2023-10-25 16:03:19 +0000

    random card generator V1

--- src/app.js
+++ src/app.js
@@ -2,10 +2,41 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// window.onload = function() {
+//   //write your code here
+
+//   let randomNumber = Math.floor(Math.random() * 12 + 1);
+
+//   console.log(randomCardSuites);
+// };
+
+// document.getElementsByClassName("upLeft")[0].innerHTML = "♥";
+
+// document.getElementsByClassName("upLeft")[0].innerHTML = "♠";
+
+// document.getElementsByClassName("upLeft")[0].innerHTML = "♣";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomCardSuites = Math.floor(Math.random() * 4 + 1);
+  if (randomCardSuites == 1) {
+    document.getElementsByClassName("upLeft")[0].innerHTML = "♥";
+    document.getElementsByClassName("downRight")[0].innerHTML = "♥";
+    document.getElementsByClassName("upLeft")[0].style.color = "red";
+    document.getElementsByClassName("downRight")[0].style.color = "red";
+  } else if (randomCardSuites == 2) {
+    document.getElementsByClassName("upLeft")[0].innerHTML = "♠";
+    document.getElementsByClassName("downRight")[0].innerHTML = "♠";
+  } else if (randomCardSuites == 3) {
+    document.getElementsByClassName("upLeft")[0].innerHTML = "♣";
+    document.getElementsByClassName("downRight")[0].innerHTML = "♣";
+  } else if (randomCardSuites == 4) {
+    document.getElementsByClassName("upLeft")[0].innerHTML = "♦";
+    document.getElementsByClassName("downRight")[0].innerHTML = "♦";
+    document.getElementsByClassName("upLeft")[0].style.color = "red";
+    document.getElementsByClassName("downRight")[0].style.color = "red";
+  }
+
+  document.getElementsByClassName("cardNumber")[0].innerHTML = Math.floor(
+    Math.random() * 12 + 1
+  );
 };
